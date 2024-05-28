14325ffa2ef63205d47002d8c0f333e43cdc6bec
unknown <hoon81@gmail.com>
2024-05-28 15:20:07 -0400

    Add insert and delete item functions

--- bst.js
+++ bst.js
@@ -22,6 +22,48 @@ class Tree {
 
     return root;
   }
+
+  insert (value, root = this.root) {
+    if (root === null) {
+      root = new Node(value);
+      return root;
+    }
+
+    if (value < root.data) {
+      root.left = this.insert(value, root.left);
+    } else if (value > root.data) {
+      root.right = this.insert(value, root.right);
+    }
+    return root;
+  }
+
+  deleteItem(value, root = this.root) {
+    if (root === null) return null;
+
+    if (value < root.data) {
+      root.left = this.deleteItem(value, root.left);
+    } else if (value > root.data) {
+      root.right = this.deleteItem(value, root.right);
+    } else {
+      if (root.left === null) return root.right;
+      else if (root.right === null) return root.left;
+
+      root.data = this.minValue(root.right);
+      root.right = this.deleteItem(root.data, root.right);
+    }
+    
+    return root;
+  }
+
+  minValue (node) {
+    let minv = node.data;
+    while (node.left !== null) {
+      minv = node.left.data;
+      node = node.left;
+    }
+    return minv;
+  }
+
 }
 
 // provided utility print function
@@ -40,4 +82,8 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 
 const dataArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const tree = new Tree(dataArray);
+prettyPrint(tree.root);
+tree.insert(15);
+prettyPrint(tree.root);
+tree.deleteItem(4);
 prettyPrint(tree.root);
